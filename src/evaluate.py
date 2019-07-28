157fe8e89062de100d81d89eed7f577b9d1def32
Sebastin Santy <t-sesan@microsoft.com>
2019-07-28 23:33:55 +0530

    Integrating sklearn-hierarchy

--- src/evaluate.py
+++
@@ -1,8 +0,0 @@
-import nltk
-
-hypothesis = "baseball player is throwing ball in game"
-reference = "Pitch a baseball"
-reference = reference.split(" ")
-hypothesis = hypothesis.split(" ")
-BLEUscore = nltk.translate.bleu_score.sentence_bleu([reference], hypothesis, weights = [1])
-print(BLEUscore)
